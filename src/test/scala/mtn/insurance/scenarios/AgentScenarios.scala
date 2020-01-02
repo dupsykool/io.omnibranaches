49835e2e6476b31f2b0b8cffa8b07e2cc6eb4e19
Dupsykool <mladejebi@swifta.com>
2020-01-02 10:42:52 +0100

    updates_today

--- src/test/scala/mtn/insurance/scenarios/AgentScenarios.scala
+++ src/test/scala/mtn/insurance/scenarios/AgentScenarios.scala
@@ -5,12 +5,16 @@ import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
 import mtn.insurance.requests.{AgentServices, BillPaymentRequest, GetTokenRequest}
 
+import scala.concurrent.duration.Duration
+
 object AgentScenarios {
+
+  val duration = Duration(100,"millis")
 
   val SuperAgentTrxn: ScenarioBuilder = scenario("Agent Transaction Simulation")
     .exec(GetTokenRequest.super_agent_get_token)
     .exec(GetTokenRequest.get_token)
-    .repeat(4) {
+    .during(duration) {
       exec(AgentServices.performWalletTransfer)
       .exec(BillPaymentRequest.agentBankTransfer)
       .exec(BillPaymentRequest.superAgentBankTransfer)
