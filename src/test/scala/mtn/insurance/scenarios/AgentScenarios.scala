ecaa8ab212c3764f346ce7b622b0f8142ce336fb
Dupsykool <mladejebi@swifta.com>
2020-01-02 13:39:18 +0100

    updates_today

--- src/test/scala/mtn/insurance/scenarios/AgentScenarios.scala
+++ src/test/scala/mtn/insurance/scenarios/AgentScenarios.scala
@@ -11,23 +11,54 @@ object AgentScenarios {
 
   val duration = Duration(100,"millis")
 
-  val SuperAgentTrxn: ScenarioBuilder = scenario("Agent Transaction Simulation")
+  val SuperAgentTrxn: ScenarioBuilder = scenario("SuperAgentTrxn Transaction Simulation")
     .exec(GetTokenRequest.super_agent_get_token)
+  .exec(AgentServices.performWalletTransfer)
+            .exec(BillPaymentRequest.agentBankTransfer)
+            .exec(BillPaymentRequest.superAgentBankTransfer)
+            .exec(AgentServices.performCashOut)
+            .exec(AgentServices.performWalletTransfer)
+//    .exec(GetTokenRequest.get_token)
+//    .exec(GetTokenRequest.get_token_1)
+//    .repeat(20) {
+//      during(duration) {
+//        randomSwitch(
+//                20.0 -> exec(AgentServices.performWalletTransfer),
+//          10.0 -> exec(AgentServices.performCashOut_1),
+//          10.0 -> exec(BillPaymentRequest.agentBankTransfer),
+//          10.0 -> exec(BillPaymentRequest.superAgentBankTransfer),
+//          20.0 -> exec(BillPaymentRequest.agentBankTransfer_1),
+//          10.0 -> exec(AgentServices.performWalletTransfer),
+//          20.0 -> exec(AgentServices.performCashOut)
+//              )
+//      }
+//    }
+
+  val AgentTrxn: ScenarioBuilder = scenario("AgentTrxn Transaction Simulation")
     .exec(GetTokenRequest.get_token)
+    .exec(BillPaymentRequest.agentBankTransfer)
+    .exec(AgentServices.performCashOut)
+//    .exec(GetTokenRequest.super_agent_get_token)
+//    .exec(GetTokenRequest.get_token)
+//    .exec(GetTokenRequest.get_token_1)
+//    .repeat(20) {
+//      during(duration) {
+//        randomSwitch(
+//          20.0 -> exec(AgentServices.performWalletTransfer),
+//          10.0 -> exec(AgentServices.performCashOut_1),
+//          10.0 -> exec(BillPaymentRequest.agentBankTransfer),
+//          10.0 -> exec(BillPaymentRequest.superAgentBankTransfer),
+//          20.0 -> exec(BillPaymentRequest.agentBankTransfer_1),
+//          10.0 -> exec(AgentServices.performWalletTransfer),
+//          20.0 -> exec(AgentServices.performCashOut)
+//        )
+//      }
+//    }
+
+  val AgentTrxn_1: ScenarioBuilder = scenario("AgentTrxn_2 Transaction Simulation")
     .exec(GetTokenRequest.get_token_1)
-    .repeat(20) {
-      during(duration) {
-        randomSwitch(
-                20.0 -> exec(AgentServices.performWalletTransfer),
-          10.0 -> exec(AgentServices.performCashOut_1),
-          10.0 -> exec(BillPaymentRequest.agentBankTransfer),
-          10.0 -> exec(BillPaymentRequest.superAgentBankTransfer),
-          20.0 -> exec(BillPaymentRequest.agentBankTransfer_1),
-          10.0 -> exec(AgentServices.performWalletTransfer),
-          20.0 -> exec(AgentServices.performCashOut)
-              )
-      }
-    }
+    .exec(BillPaymentRequest.agentBankTransfer_1)
+    .exec(AgentServices.performCashOut_1)
 
 //  val SuperAgentTrxn: ScenarioBuilder = scenario("Agent Transaction Simulation")
 //    .exec(GetTokenRequest.super_agent_get_token)
--- src/test/scala/mtn/insurance/simulations/AgentTransactionSimulation.scala
+++ src/test/scala/mtn/insurance/simulations/AgentTransactionSimulation.scala
@@ -1,16 +1,21 @@
 package mtn.insurance.simulations
 
 import io.gatling.core.Predef._
+
 import scala.concurrent.duration._
 import mtn.insurance.config.Config
 import mtn.insurance.config.Config.users
+import mtn.insurance.scenarios
 import mtn.insurance.scenarios.AgentScenarios
 
 class AgentTransactionSimulation extends Simulation {
 
   setUp(
 //    AgentScenarios.Agent_login.inject(atOnceUsers(1)),
-    AgentScenarios.SuperAgentTrxn.inject(atOnceUsers(users))
+    AgentScenarios.SuperAgentTrxn.inject(constantUsersPerSec(5).during(2 seconds)),
+    AgentScenarios.AgentTrxn.inject(constantUsersPerSec(10).during(2 seconds)),
+    AgentScenarios.AgentTrxn_1.inject(constantUsersPerSec(10).during(2 seconds))
+
 //    AgentScenarios.SuperAgentTrxn.inject(constantUsersPerSec(100).during(60 seconds))
   )
 
