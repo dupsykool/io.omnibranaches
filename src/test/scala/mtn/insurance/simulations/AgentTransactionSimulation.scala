60401a30e9d7465088eec12ebac7032d1d72ab97
Dupsykool <mladejebi@swifta.com>
2020-01-03 15:29:12 +0100

    updates_today

--- src/test/scala/mtn/insurance/simulations/AgentTransactionSimulation.scala
+++ src/test/scala/mtn/insurance/simulations/AgentTransactionSimulation.scala
@@ -19,8 +19,8 @@ class AgentTransactionSimulation extends Simulation {
 //    AgentScenarios.Agent_login.inject(atOnceUsers(1)),
 //    AgentScenarios.SuperAgentTrxn.inject(atOnceUsers(1))
     AgentScenarios.SuperAgentTrxn.inject(atOnceUsers(1)),
-      AgentScenarios.AgentTrxn.inject(atOnceUsers(1)),
-  AgentScenarios.AgentTrxn_1.inject(atOnceUsers(1))
+      AgentScenarios.AgentTrxn.inject(constantUsersPerSec(5).during(2 seconds)),
+  AgentScenarios.AgentTrxn_1.inject(constantUsersPerSec(3).during(2 seconds))
 //    AgentScenarios.AgentTrxn.inject(constantUsersPerSec(10).during(2 seconds)),
 //    AgentScenarios.AgentTrxn_1.inject(constantUsersPerSec(10).during(2 seconds))
 
